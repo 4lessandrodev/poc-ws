26d2a1aff4973def1d9ff06c0cd2ad66f974a5f4
4lessandrodev <alessandroadm@live.com>
2024-02-23 16:58:29 -0300

    feat: added payload and event name'

--- on-create-schedule.event.js
+++ on-create-schedule.event.js
@@ -1,30 +1,32 @@
 const { appendFileSync } = require('fs');
 
 module.exports = onCreateSchedule = (doctorId, schedule, clients) => {
+    var startMemory = process.memoryUsage().heapUsed;
+    const date = new Date().toISOString();
+
     return new Promise((resolve) => {
         const clientsToNotify = clients.filter((cl) => cl.doctorId === doctorId);
         if(clientsToNotify.length) clientsToNotify.forEach(client => {
             client.ws.send(JSON.stringify({ payload: { ...schedule }, eventName: 'schedule.created'}));
         });
 
-        var startMemory = process.memoryUsage().heapUsed;
-        const date = new Date().toISOString();
-
+        var i = 0; while (i < 1e5) { i++ };
+        var task1 = 'Criando sessão de video conferencia...';
         var endMemory = startMemory - process.memoryUsage().heapUsed;
         var memoryUsed = `${(endMemory / (1024 * 1024)).toFixed(2)}mb`
-        var task1 = 'Criando sessão de video conferencia...';
         appendFileSync('stats', JSON.stringify({ date, task: task1, memoryUsed }) + ',\n', 'utf8');
-        var i = 0; while (i < 1e5) { i++ };
 
-        var memoryUsed = `${(endMemory / (1024 * 1024)).toFixed(2)}mb`
+        var i = 0; while (i < 1e5) { i++ };
         var task2 = 'Enviando email...';
+        var endMemory = startMemory - process.memoryUsage().heapUsed;
+        var memoryUsed = `${(endMemory / (1024 * 1024)).toFixed(2)}mb`
         appendFileSync('stats', JSON.stringify({ date, task: task2, memoryUsed }) + ',\n', 'utf8');
-        var i = 0; while (i < 1e5) { i++ };
 
+        var i = 0; while (i < 1e5) { i++ };
         var task3 = 'Atualizando agendamento com sessão...';
+        var endMemory = startMemory - process.memoryUsage().heapUsed;
         var memoryUsed = `${(endMemory / (1024 * 1024)).toFixed(2)}mb`
         appendFileSync('stats', JSON.stringify({ date, task: task3, memoryUsed }) + ',\n', 'utf8');
-        var i = 0; while (i < 1e5) { i++ };
         resolve();
     })
 }
