9a588ca3b5a1c9f566e96187561347233a426798
4lessandrodev <alessandroadm@live.com>
2024-02-23 16:55:28 -0300

    feat: added payload and event name'

--- on-create-schedule.event.js
+++ on-create-schedule.event.js
@@ -2,20 +2,29 @@ const { appendFileSync } = require('fs');
 
 module.exports = onCreateSchedule = (doctorId, schedule, clients) => {
     return new Promise((resolve) => {
-        const date = new Date().toISOString();
         const clientsToNotify = clients.filter((cl) => cl.doctorId === doctorId);
         if(clientsToNotify.length) clientsToNotify.forEach(client => {
             client.ws.send(JSON.stringify({ payload: { ...schedule }, eventName: 'schedule.created'}));
         });
-        var i = 0; while (i < 1e5) { i++ };
+
+        var startMemory = process.memoryUsage().heapUsed;
+        const date = new Date().toISOString();
+
+        var endMemory = startMemory - process.memoryUsage().heapUsed;
+        var memoryUsed = `${(endMemory / (1024 * 1024)).toFixed(2)}mb`
         var task1 = 'Criando sessão de video conferencia...';
-        appendFileSync('stats', JSON.stringify({ date, task: task1 }) + ',\n', 'utf8');
+        appendFileSync('stats', JSON.stringify({ date, task: task1, memoryUsed }) + ',\n', 'utf8');
         var i = 0; while (i < 1e5) { i++ };
+
+        var memoryUsed = `${(endMemory / (1024 * 1024)).toFixed(2)}mb`
         var task2 = 'Enviando email...';
-        appendFileSync('stats', JSON.stringify({ date, task: task2 }) + ',\n', 'utf8');
+        appendFileSync('stats', JSON.stringify({ date, task: task2, memoryUsed }) + ',\n', 'utf8');
         var i = 0; while (i < 1e5) { i++ };
+
         var task3 = 'Atualizando agendamento com sessão...';
-        appendFileSync('stats', JSON.stringify({ date, task: task3 }) + ',\n', 'utf8');
+        var memoryUsed = `${(endMemory / (1024 * 1024)).toFixed(2)}mb`
+        appendFileSync('stats', JSON.stringify({ date, task: task3, memoryUsed }) + ',\n', 'utf8');
+        var i = 0; while (i < 1e5) { i++ };
         resolve();
     })
 }
